691e1fdf158827d37f4d0253de384abe42c20aa2
Imam Ahmad Qusyairi <98892253+imamaqusairi@users.noreply.github.com>
2023-05-11 11:54:54 +0700

    Add other requirements for deployment

---
+++ Other Interesting Projects/Streamlit_TUTORIAL/setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~.streamlit
+echo 
+[general]n
+email = your-email@domain.comn
+  ~.streamlitcredentials.toml
+echo 
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+  ~.streamlitconfig.toml
